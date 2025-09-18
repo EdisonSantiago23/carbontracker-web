b621220843ce76510ec2b0a221e04b2cfe389bad
EdisonSantiago23 <vicenteedison93@gmail.com>
2025-09-17 22:23:27 -0500

    rev

---
+++ unimported.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: ["src/index.js"],
+  aliases: {
+    "@components": "./src/components",
+    "@services": "./src/services",
+    "@config": "./src/config",
+    "@styles": "./src/styles",
+  },
+};
